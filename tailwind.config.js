4a0ed63c834ca5bae7ad232f387186d74dde89cb
Arnaudb78 <arnaudb92230@gmail.com>
2024-06-02 18:45:22 +0200

    WIP: begin integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.{js,js,ts,ts,html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1B1B1B",
+        secondary: {
+          100: "#E30972",
+          200: "#EEEDED",
+        },
+      },
+    },
   },
   plugins: [],
 };
