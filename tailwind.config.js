1c5036ae0665ed46a85d5df7a6f9059fc5ffd679
Arnaudb78 <arnaudb92230@gmail.com>
2024-06-14 15:01:31 +0200

    WIP: add desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{js,js,ts,ts,html}"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "couple": "url('assets/images/bg.png')",
-      },
-      colors: {
-        primary: "#232D32",
-        secondary: {
-          100: "#F091FF",
-          200: "#FEFDF4",
-          300: "#FF6419",
+    content: ["./*.{js,js,ts,ts,html}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#232D32",
+                secondary: {
+                    100: "#F091FF",
+                    200: "#FEFDF4",
+                    300: "#FF6419",
+                },
+            },
+            fontFamily: {
+                satoshi: ["Satoshi", "sans-serif"],
+            },
         },
-      },
-      fontFamily: {
-        satoshi:["Satoshi", "sans-serif"]
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
